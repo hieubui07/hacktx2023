0ad017d222d3496ad3e194142dcc49d271aba1e4
PolugaGit <77752971+PolugaGit@users.noreply.github.com>
2023-10-22 10:52:11 -0500

    Chatbot

--- pages/Chat.py
+++ pages/Chat.py
@@ -3,10 +3,8 @@ import streamlit as st
 def app():
     st.write('Home')
 
-<<<<<<< HEAD
+
 link = '[Botpress](https://mediafiles.botpress.cloud/1ead3258-715f-4967-8e76-e4279e1f3c5b/webchat/bot.html)'
-st.markdown(link, unsafe_allow_html=True)
-=======
-link = "https://mediafiles.botpress.cloud/1ead3258-715f-4967-8e76-e4279e1f3c5b/webchat/bot.htm"
+
 st.write(f"Link to Chaptbot {link}")
->>>>>>> 3aa4043447da7defca7070192fb51f42b315a2c5
+
