d79c13e36d0ea7d4afcb173a26ef51f18204cf52
hieubui07 <hieubui0107@gmail.com>
2023-10-22 13:56:39 -0500

    Update contacts

--- pages/Contacts.py
+++ pages/Contacts.py
@@ -2,3 +2,8 @@ import streamlit as st
 
 def app():
     st.write('Contacts')
+
+st.write("Emails:")
+st.write("Daniel Oake: droake2012@gmail.com")
+st.write("Hieu Bui: hieubui0107@gmail.com")
+st.write("Truc Le: trule4@my.lonestar.edu")
