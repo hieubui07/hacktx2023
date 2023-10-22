e1a6798203ba2e8ec5d97e5cfcab8ec6430513c7
hieubui07 <hieubui0107@gmail.com>
2023-10-22 10:02:32 -0500

    update main

--- main.py
+++ main.py
@@ -25,15 +25,7 @@ with st.container():
 
 with st.container():
     st.write("---------")
-    left_column,right_column = st.columns(2)
-    with left_column:
-        st.header("Pics")
-        st.write("##")
-        st.write(
-            "Searching for psychiatrists?"
-        )
-    with right_column:
-        st.header('Address and name of psychiatrists')
+    st.write("Searching for psychiatrists?")
         
 class MultiApp:
     
